961e467e078ca65f9e7ac1368dadcd81dd01010a
Diego Ernst <dernst@xmartlabs.com>
2017-08-30 15:37:39 -0300

    wip

--- examples/bunnyRigid3D.py
+++ examples/bunnyRigid3D.py
@@ -26,28 +26,85 @@ def visualize(iteration, error, X, Y, ax):
     print("iteration %d, error %.5f" % (iteration, error))
     plt.pause(0.001)
 
+
+yaw = 0
+pitch = 0
+roll = 0
+x = 0
+y = 0
+z = 0
+
+fish = loadmat('./data/bunny.mat')
+X = fish['X']
+
+R = rotation_matrix(yaw, 0, 0)
+Y = X.dot(R)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+def redraw():
+    global yaw, pitch, roll
+    global X, Y
+    global x, y ,z
+    global plt, ax
+    centroid = np.mean(X, axis=0)
+    R = rotation_matrix(yaw, pitch, roll)
+    T = np.array([x, y ,z])
+    Y = (X - centroid).dot(R) + centroid + T
+    plt.cla()
+    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red')
+    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue')
+    plt.draw()
+
+def manage_key_pressed(event):
+    print(event.key)
+    global yaw, roll, pitch
+    global x, y, z
+    if event.key == "i":
+        yaw += 0.05
+    elif event.key == "j":
+        yaw -= 0.05
+    elif event.key == "o":
+        roll += 0.05
+    elif event.key == "k":
+        roll -= 0.05
+    elif event.key == "p":
+        pitch += 0.05
+    elif event.key == "l":
+        pitch -= 0.05
+    if event.key == "I":
+        x += 0.05
+    elif event.key == "J":
+        x -= 0.05
+    elif event.key == "O":
+        y += 0.05
+    elif event.key == "K":
+        y -= 0.05
+    elif event.key == "P":
+        z += 0.05
+    elif event.key == "L":
+        z -= 0.05
+    redraw()
+
+
+
+
 def main():
-    fish = loadmat('./data/bunny.mat')
+    # draw
+    plt.cla()
+    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red')
+    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue')
+    plt.draw()
 
-    R = rotation_matrix(np.pi / 2, np.pi / 4, np.pi / 18)
-    # noinspection PyTypeChecker
-    np.testing.assert_approx_equal(np.linalg.det(R), 1)
-    T = np.array([1, 2, 3])
-
-    X = fish['X']
-    # Y = X + 1
-    Y = X.dot(R)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    cid = plt.gcf().canvas.mpl_connect('key_press_event', manage_key_pressed)
 
 
-    callback = partial(visualize, ax=ax)
+    # callback = partial(visualize, ax=ax)
+    # reg = rigid_registration(X, Y, maxIterations=1000)
+    # reg.register(callback)
 
-    reg = rigid_registration(X, Y, maxIterations=1000)
-    reg.register(callback)
     plt.show()
-
 
 if __name__ == '__main__':
     main()
